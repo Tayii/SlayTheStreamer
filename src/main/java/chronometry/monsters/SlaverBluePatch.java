e90870e8b0ac28eee5132df4c855cce72247d561
Tayii <tai_saito@mail.ru>
2019-03-17 01:26:12 +0500

    added SlaverBlue and SlaverRed

---
+++ src/main/java/chronometry/monsters/SlaverBluePatch.java
@@ -0,0 +1,48 @@
+package chronometry.monsters;
+
+import basemod.ReflectionHacks;
+import chronometry.IntentData;
+import chronometry.effects.AttackEffect;
+import chronometry.effects.DebuffWeakEffect;
+import chronometry.patches.AbstractMonsterPatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster.Intent;
+import com.megacrit.cardcrawl.monsters.exordium.SlaverBlue;
+
+import java.util.ArrayList;
+
+public class SlaverBluePatch {
+    @SpirePatch(clz=SlaverBlue.class,
+                method=SpirePatch.CONSTRUCTOR,
+                paramtypez = {float.class, float.class})
+    public static class InitMoves {
+        public static void Postfix(SlaverBlue __instance, float x, float y) {
+            ArrayList<IntentData> moves = new ArrayList<IntentData>();
+
+            IntentData move1 = new IntentData(
+                    __instance.getClass(),
+                    "STAB",
+                    Intent.ATTACK
+            );
+            move1.add_effect(new AttackEffect(__instance.damage.get(0)));
+            moves.add(move1);
+
+            IntentData move4 = new IntentData(
+                    __instance.getClass(),
+                    "RAKE",
+                    Intent.ATTACK_DEBUFF,
+                    __instance.MOVES[0]
+            );
+            int weakAmt = (int)ReflectionHacks.getPrivate(__instance, __instance.getClass(), "weakAmt");
+            if (AbstractDungeon.ascensionLevel >= 17) {
+                weakAmt += 1;
+            }
+            move4.add_effect(new AttackEffect(__instance.damage.get(1)));
+            move4.add_effect(new DebuffWeakEffect(weakAmt));
+            moves.add(move4);
+
+            AbstractMonsterPatch.intent_moves.set(__instance, moves);
+        }
+    }
+}
---
+++ src/main/java/chronometry/monsters/SlaverRedPatch.java
@@ -0,0 +1,63 @@
+package chronometry.monsters;
+
+import basemod.ReflectionHacks;
+import chronometry.IntentData;
+import chronometry.effects.AttackEffect;
+import chronometry.effects.DebuffVulnerableEffect;
+import chronometry.effects.UniqueEffect;
+import chronometry.patches.AbstractMonsterPatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster.Intent;
+import com.megacrit.cardcrawl.monsters.exordium.SlaverRed;
+
+import java.util.ArrayList;
+
+public class SlaverRedPatch {
+    @SpirePatch(clz=SlaverRed.class,
+                method=SpirePatch.CONSTRUCTOR,
+                paramtypez = {float.class, float.class})
+    public static class InitMoves {
+        public static void Postfix(SlaverRed __instance, float x, float y) {
+            ArrayList<IntentData> moves = new ArrayList<IntentData>();
+
+            IntentData move1 = new IntentData(
+                    __instance.getClass(),
+                    "STAB",
+                    Intent.ATTACK
+            );
+            move1.add_effect(new AttackEffect(__instance.damage.get(0)));
+            moves.add(move1);
+
+            IntentData move2 = new IntentData(
+                    __instance.getClass(),
+                    "ENTANGLE",
+                    Intent.ATTACK,
+                    (String)ReflectionHacks.getPrivateStatic(__instance.getClass(), "ENTANGLE_NAME"),
+                    1
+            );
+            move2.add_effect(new UniqueEffect(
+                    __instance.getClass(),
+                    "UNIQUE_1",
+                    1
+            ));
+            moves.add(move2);
+
+            IntentData move3 = new IntentData(
+                    __instance.getClass(),
+                    "SCRAPE",
+                    Intent.ATTACK_DEBUFF,
+                    (String)ReflectionHacks.getPrivateStatic(__instance.getClass(), "SCRAPE_NAME")
+            );
+            int VULN_AMT = (int)ReflectionHacks.getPrivate(__instance, __instance.getClass(), "VULN_AMT");
+            if (AbstractDungeon.ascensionLevel >= 17) {
+                VULN_AMT += 1;
+            }
+            move3.add_effect(new AttackEffect(__instance.damage.get(1)));
+            move3.add_effect(new DebuffVulnerableEffect(VULN_AMT));
+            moves.add(move3);
+
+            AbstractMonsterPatch.intent_moves.set(__instance, moves);
+        }
+    }
+}
