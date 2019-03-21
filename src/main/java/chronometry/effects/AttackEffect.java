e220568a90eec5cb6b614b1cd4eb8462659e7376
Tayii <tai_saito@mail.ru>
2019-03-21 13:27:09 +0500

    made AttackEffect support links to multiplier variable

--- src/main/java/chronometry/effects/AttackEffect.java
+++ src/main/java/chronometry/effects/AttackEffect.java
@@ -1,10 +1,14 @@
 package chronometry.effects;
 
+import basemod.ReflectionHacks;
 import chronometry.MoveEffect;
 import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 public class AttackEffect extends MoveEffect {
     public DamageInfo info;
+    public AbstractMonster monster;
+    public String multiplierVariableName = null;
 
     public AttackEffect(DamageInfo damage) {
         this(damage, 1);
@@ -15,7 +19,22 @@ public class AttackEffect extends MoveEffect {
         this.multiplier = multiplier;
     }
 
+    public AttackEffect(DamageInfo damage, AbstractMonster monster, String variableName) {
+        this.info = damage;
+        this.monster = monster;
+        this.multiplierVariableName = variableName;
+    }
+
     public int number() {
         return this.info.output;
+    }
+
+    public int multiplier() {
+        if (multiplierVariableName != null) {
+            return (int) ReflectionHacks.getPrivate(this.monster, this.monster.getClass(), this.multiplierVariableName);
+        }
+        else {
+            return this.multiplier;
+        }
     }
 }
